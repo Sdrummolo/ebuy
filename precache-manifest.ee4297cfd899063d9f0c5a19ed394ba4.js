250d55064a500c4df28579b9622affd90135faa1
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-05-10 21:56:42 +0100

    Updates

---
+++ precache-manifest.ee4297cfd899063d9f0c5a19ed394ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c16a8d10b40bac73442d89b3298f21",
+    "url": "/ebuy/index.html"
+  },
+  {
+    "revision": "24f111b4f448533b00c7",
+    "url": "/ebuy/static/css/main.0c2efd07.chunk.css"
+  },
+  {
+    "revision": "3f3338f5dd418907c4f6",
+    "url": "/ebuy/static/js/2.85e0ef13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ebuy/static/js/2.85e0ef13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f111b4f448533b00c7",
+    "url": "/ebuy/static/js/main.15b85915.chunk.js"
+  },
+  {
+    "revision": "9afdedc4b9ca41fe34e0",
+    "url": "/ebuy/static/js/runtime-main.40bec528.js"
+  }
+]);
